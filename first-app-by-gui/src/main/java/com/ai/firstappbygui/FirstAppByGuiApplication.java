ab6d88cb070b7ab841b6a5260c6f4702700a210c
bjp-yxkj-gul <touchjava@126.com>
2019-08-02 22:19:52 +0800

    理解固化的Maven依赖

---
+++ first-app-by-gui/src/main/java/com/ai/firstappbygui/FirstAppByGuiApplication.java
@@ -0,0 +1,14 @@
+package com.ai.firstappbygui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.loader.JarLauncher;
+
+@SpringBootApplication
+public class FirstAppByGuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstAppByGuiApplication.class, args);
+	}
+
+}
